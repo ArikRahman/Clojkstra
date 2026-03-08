ac3f34470ca89d3d7392236e6b63415f5f660e37
ArikRahman <arik@tamu.edu>
2026-03-08 00:16:46 -0600

    Updated to react 19 and updated dependencies

--- src/clojkstra/app/core.cljs
+++ src/clojkstra/app/core.cljs
@@ -1,15 +1,17 @@
 (ns clojkstra.app.core
   (:require
-   [reagent.dom          :as rdom]
+   [reagent.dom.client   :as rdom]
    [re-frame.core        :as rf]
    [clojkstra.app.events :as events]
    [clojkstra.app.subs   :as subs]
    [clojkstra.app.routes :as routes]
    [clojkstra.app.views  :as views]))
 
+(defonce root (rdom/create-root (.getElementById js/document "app")))
+
 (defn- mount-root []
   (rf/clear-subscription-cache!)
-  (rdom/render [views/app-root] (.getElementById js/document "app")))
+  (rdom/render root [views/app-root]))
 
 (defn init []
   (rf/dispatch-sync [::events/initialize-db])
